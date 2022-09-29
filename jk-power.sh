d5d3be4886b86b5741403748c7940cd49bdd7530
Jon Kracht <jonkracht@gmail.com>
2022-09-29 14:37:31 -0400

    Editting jk-power

--- jk-power.sh
+++ jk-power.sh
@@ -1,17 +1,14 @@
 #!/bin/bash
 
-primary=eDP-1
-secondary=HDMI-1
-
 action=$(dmenu -fn 'FiraSans-Bold' -i -m 0 -p "Power options: "<< EOM
+lockscreen
 reboot
-screenlocker
 shutdown
 suspend
 EOM
 )
 
-if [[ "$action" = "shutdown now" ]]
+if [[ "$action" = "shutdown" ]]
 then
     exec shutdown -h now
 elif [[ "$action" = "suspend" ]]
@@ -20,7 +17,7 @@ then
 elif [[ "$action" = "reboot" ]]
 then 
     exec shutdown -r now
-elif [[ "$action" = "screenlocker" ]]
+elif [[ "$action" = "lockscreen" ]]
 then 
     exec i3lock --image=/mnt/1-tb-hd/art/paul-klee/Theater-Mountain-Construction-cropped.png --tiling --show-failed-attempts
 fi
