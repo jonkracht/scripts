9a917de4bd0b945fe4424201d532afa62d5c40e2
Jon Kracht <jonkracht@gmail.com>
2022-10-12 15:14:34 -0400

    October push

--- jk-power.sh
+++ jk-power.sh
@@ -2,7 +2,7 @@
 
 
 
-action=$('$Dmenu_Base_Cmd' -p "Power options: "<< EOM
+action=$(dmenu $dmenu_flags -p "Power options: "<< EOM
 lockscreen
 reboot
 shutdown
@@ -13,16 +13,15 @@ EOM
 if [[ "$action" = "shutdown" ]]
 then
     exec shutdown -h now
-elif [[ "$action" = "suspend" ]]
 
+elif [[ "$action" = "suspend" ]]
 then
     exec systemctl suspend
 elif [[ "$action" = "reboot" ]]
-
 then 
     exec shutdown -r now
 elif [[ "$action" = "lockscreen" ]]
-
 then 
     exec $LockScreenCmd
+
 fi
--- monitor-setup.sh
+++ monitor-setup.sh
@@ -3,7 +3,7 @@
 primary=eDP-1
 secondary=HDMI-1
 
-action=$(dmenu -i -fn $FONT -m 0 -p "Action: "<< EOM
+action=$(dmenu $dmenu_flags -p "Action: "<< EOM
 HDMI-off
 HDMI-on
 EOM
