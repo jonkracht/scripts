8a47c14496a4ce6134c0abf55f337152c631d773
Jon Kracht <jonkracht@gmail.com>
2025-07-21 11:27:15 -0400

    Handling x and wayland usage

--- jk-power.sh
+++ jk-power.sh
@@ -2,13 +2,26 @@
 
 # Check if dmenu_flags is set; if not, set it
 if [[ -v dmenu_flags ]]; then
-echo "dmenu_flags exists."
+echo "dmenu_flags exists and is set to:  $dmenu_flags"
 else
-echo "dmenu_flags does not exist. setting."
 dmenu_flags="-i -fn RobotoMonoNerdFont-Regular"
+echo "dmenu_flags does not exist. Setting to:  $dmenu_flags"
 fi
 
-action=$(dmenu $dmenu_flags -p "Power options: "<< EOM
+
+# Handle both x and wayland where dmenu executable may be differently name
+if [[ "$XDG_SESSION_TYPE" = "x11" ]]; then
+    echo "X"
+    dmenu_bin="dmenu"
+elif [[ "$XDG_SESSION_TYPE" = "wayland" ]]; then
+    echo "Wayland"
+    dmenu_bin="dmenu_wl"
+else
+    echo "Session type not handled:  $XDG_SESSION_TYPE"
+fi
+
+
+action=$($dmenu_bin $dmenu_flags -p "Power options: "<< EOM
 suspend
 reboot
 shutdown
@@ -17,20 +30,20 @@ screensaver
 EOM
 )
 
-if [[ "$action" = "shutdown" ]]
-then
+# Options
+if [[ "$action" = "shutdown" ]]; then
     exec shutdown -h now
 
-elif [[ "$action" = "suspend" ]]
-then
+elif [[ "$action" = "suspend" ]]; then
     exec systemctl suspend
-elif [[ "$action" = "reboot" ]]
-then 
+
+elif [[ "$action" = "reboot" ]]; then 
     exec shutdown -r now
-elif [[ "$action" = "lockscreen" ]]
-then 
+
+elif [[ "$action" = "lockscreen" ]]; then 
     exec $lockScreenCmd
-elif [[ "$action" = "screensaver" ]]
-then 
+
+elif [[ "$action" = "screensaver" ]]; then 
     exec xscreensaver-command --activate
+
 fi
